c8633c8b2500eaa4efdca0a67f8d7b7111cfb17c
A R Shahrear <arshahrear30@gmail.com>
2025-09-19 23:18:46 +0600

    Update DatabaseHelper.java

--- DatabaseHelper.java
+++ DatabaseHelper.java
@@ -11,10 +11,19 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public final static String DB_NAME="DB_name";
     public final static int DB_VERSION=1;
 
+    //DB_VERSION Upgrade হলে onUpgrade call হয় ।
+    
+    //3018
+    // DB_VERSION=2, Upgrade হলে onCreate এর table আগের গুলো Delete করতে হয় ।তখন
+    //db.execSQL("drop table if exists my_table"); লিখতে হবে onUpgrade এর ভিতরে ।
+    //যত গুলো table থাকবে ততবার লিখতে হবে same code ।
+
 
     public DatabaseHelper(@Nullable Context context) {
         super(context, DB_NAME, null, 1);
     }
+    
+    //3017
     //context হলো যেই class এ কাজ করা হয় ঐ class এর environment যেমন : DatabaseHelper
     //String name এখানে name টা হলো database name
     //int version হলো database এর version
@@ -31,5 +40,14 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+
     }
+
+
+    public void insertData(){
+
+        
+    }
+
+
 }
